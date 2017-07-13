2b052178f46ae1dc73a021c7ea3c51d0e9f871b8
unknown <>
2017-07-14 05:04:50 +0900

    realized makeJSON function lightly

--- tapad/preset-store/create/index.js
+++ tapad/preset-store/create/index.js
@@ -1,4 +1,15 @@
-var json_str = JSON.stringify(data3);
 function makeJSON(){
-  for(var i = 0;i<)
+  var jsonObject = [
+    {"song_title" : $("#song_title").val()},
+    {"song_preset_creator" : $("#song_preset_creator").val()}, //make JSON manually because HTML's inputs are customized
+    {"song_theme_color" : $("#song_theme_color").val()},
+    {"song_tutorial_link" : $("#song_tutorial_link").val()},
+    {"song_bpm" : $("#song_bpm").val()}
+  ]
+  var jsonStr = JSON.stringify(jsonObject); //change object into String
+  console.log(jsonStr);
+}
+
+function createPreset(){ //if create button clicked
+  makeJSON();
 }
