f6ddcd8c8f78b304a6d4be77dc3929b0e3b19fba
minjuho <joey91426@gmail.com>
2017-07-19 10:12:13 +0900

    preview test

--- tapad/preset-store/index.js
+++ tapad/preset-store/index.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
-  $.ajax({
-    type: "GET",
-    url: "https://firebasestorage.googleapis.com/v0/b/tapad-4d342.appspot.com/o/presets%2Fmetadata?alt=media",
-      success: function(data){
-        var testjson = eval(data);
-        document.getElementById("preset_name").innerHTML += testjson[0].about.title;
-      }
-  })
-})
+  $.getJSON("https://firebasestorage.googleapis.com/v0/b/tapad-4d342.appspot.com/o/presets%2Fmetadata?alt=media",
+    function(data){
+      alert("success");
+    }
+  );
+});
