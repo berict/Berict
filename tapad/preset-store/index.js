647da6a0687b288234b2f165f58689ad819ff3b2
BedrockDev <dev.bedrock@gmail.com>
2017-08-10 20:21:23 +0900

    Modified stylesheet syntax

--- tapad/preset-store/index.js
+++ tapad/preset-store/index.js
@@ -1,7 +1,13 @@
-$(document).ready(function(){
-  $.getJSON("https://firebasestorage.googleapis.com/v0/b/tapad-4d342.appspot.com/o/presets%2Fmetadata?alt=media",
-    function(data){
-      alert("success");
+$(document).ready(
+    function () {
+        $.ajax({
+            url: "https://firebasestorage.googleapis.com/v0/b/tapad-4d342.appspot.com/o/presets%2Fmetadata?alt=media&token=c2e8e053-397d-484b-87b0-f84ef5e9e083",
+            success: function (result) {
+                alert(result);
+            },
+            error: function () {
+                alert("unji");
+            }
+        });
     }
-  );
-});
+);
