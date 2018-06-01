49c847dd4fe70071f56157e5a93f59aefd033c7a
BedrockDev <dev.bedrock@gmail.com>
2018-06-01 20:49:56 +0900

    Modified nginx config

--- file/nginx/apply.sh
+++ file/nginx/apply.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo Fetching latest commit.
 cd /berict
 git pull
+
+echo Updating nginx config.
 cp -v /berict/file/nginx/default /etc/nginx/sites-enabled/default
+
+echo Restarting nginx.
 nginx -s reload
+
+echo Finished.
